5e09375d81698f1497cb5ea870de42c81a944f0c
Tomek K <tomek.krogulec@o2.pl>
2020-11-27 13:00:00 +0100

    change indexes

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -1,3 +1,3 @@
-Create index employee_idx on employees (id);
-Create index passenger_idx on passengers (id);
-Create index plane_idx on planes (id);
+Create index employee_idx on employees (name);
+Create index passenger_idx on passengers (name);
+Create index plane_idx on planes (model);
